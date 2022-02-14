fa047ca05f04a2dbcca9cc11ca56e56f02bce465
StephaneSCC <clovisstephane66@gmail.com>
2022-02-14 11:29:15 -0400

    finished vid copy

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function intTo2Digits(val) {
 }
 
 let weather = {
-    "apiKey": "b351858604ee4f8c51b7457dcf255156"
+    "apiKey": "b351858604ee4f8c51b7457dcf255156",
     fetchweather: function (city) {
         fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey).then((response) => response.json())
             .then((data) => this.displayWeather(data));
@@ -38,8 +38,27 @@ let weather = {
         const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
-        console.log(name, icon, description, temp, humidity, speed)
-        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".city").innerText = "La méteo au " + name;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind Speed: " + speed + "km/h";
+        document.querySelector(".weather").classList.remove("loading");
+    },
+    search: function () {
+        this.fetchweather(document.querySelector(".search-bar").value);
     }
 };
+
+document.querySelector(".search button").addEventListener("click", function () {
+    weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+        weather.search();
+    }
+});
+
+weather.fetchweather("Le Lamentin");
