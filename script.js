cfb29425727c1164180c29d9cb2647fa62b10b76
StephaneSCC <clovisstephane66@gmail.com>
2022-02-16 12:00:31 -0400

    improved visibility and added logos

--- script.js
+++ script.js
@@ -10,9 +10,9 @@ function display_ct() {
 }
 
 function dateToddmmyyyy(date) {
-    document.getElementById('ct').style.fontSize = '50px';
-    document.getElementById('ct').style.justifyContent = 'center';
-    document.getElementById('ct').style.fontFamily = "Impact,Charcoal,sans-serif";
+    document.getElementById('ct').style.fontSize = '70px';
+    document.getElementById('ct').style.fontStyle = 'italic';
+    document.getElementById('ct').style.fontFamily = "Times New Roman, Times, serif";
 
     var d = date.getDate();
     var month = date.getMonth() + 1; //Month from 0 to 11
@@ -30,7 +30,8 @@ function intTo2Digits(val) {
 let weather = {
     "apiKey": "b351858604ee4f8c51b7457dcf255156",
     fetchweather: function (city) {
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&lang=fr&appid=" + this.apiKey).then((response) => response.json())
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&lang=fr&appid=" + this.apiKey)
+            .then((response) => response.json())
             .then((data) => this.displayWeather(data));
     },
     displayWeather: function (data) {
@@ -39,13 +40,13 @@ let weather = {
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
         const { pressure } = data.main;
-        document.querySelector(".city").innerText = "La méteo au " + name;
+        document.querySelector(".city").innerText = "La méteo: " + name;
         document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
         document.querySelector(".description").innerText = description;
         document.querySelector(".temp").innerText = temp + "°C";
         document.querySelector(".humidity").innerText = "Humidité: " + humidity + "%";
         document.querySelector(".wind").innerText = "Vitesse du vent: " + speed + " km/h";
-        document.querySelector(".pressure").innerText = "Pression atmo: " + pressure + " atm";
+        document.querySelector(".pressure").innerText = "Pression atmo: " + pressure + " hpa";
         document.querySelector(".weather").classList.remove("loading");
     },
     search: function () {
@@ -64,3 +65,19 @@ document.querySelector(".search-bar").addEventListener("keyup", function (event)
 });
 
 weather.fetchweather("Le Lamentin");
+
+let weather2 = {
+    fetchweather: function (data) {
+        fetch("https://services1.arcgis.com/y8pKCLYeLI1K2217/arcgis/rest/services/ind_martinique/FeatureServer/0/query?where=code_zone%20%3D%20%2797213%27&outFields=date_ech,code_qual,lib_qual,coul_qual,date_dif,source,type_zone,code_zone,lib_zone&outSR=4326&f=json")
+            .then((response) => response.json())
+            .then((data) => this.displayWeather(data));;
+    },
+    displayWeather: function (data) {
+        const { name } = data;
+        const { lib_qual, coul_qual } = data.features[0].attributes;
+        console.log(data.features[0].attributes);
+        document.querySelector(".lib_qual").innerText = "Qualité d'air: " + lib_qual + " " + coul_qual;
+    },
+}
+
+weather2.fetchweather("Le Lamentin");
